dfd0960fc42222bea8ee49c01522d21a0b7c0621
josephmathew900 <josephmathew900@gmail.com>
2021-02-07 08:53:07 +0530

    useEffect after render

---
+++ src/components/HookCounterOne.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+
+export default function HookCounterOne() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
+
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>Click {count} times</button>
+    </div>
+  );
+}
