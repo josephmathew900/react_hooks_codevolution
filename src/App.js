cc3aa65c8dd379bb9b623afc56c8173bef6b6fbf
josephmathew900 <josephmathew900@gmail.com>
2021-02-08 22:59:51 +0530

    useDocumentTitle custom hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import React, { useReducer } from "react";
-import ClassTimer from "./components/useRef/ClassTimer";
-import HookTimer from "./components/useRef/HookTimer";
+import DocTitleOne from "./components/customHook/DocTitleOne";
+import DocTitleTwo from "./components/customHook/DocTitleTwo";
 
 function App() {
   return (
     <div className="App">
-      <ClassTimer />
-      <HookTimer />
+      <DocTitleOne />
+      <DocTitleTwo />
     </div>
   );
 }
